cfbc0006e509c6f71e12313d47fc24baf5a29a4c
peaky76 <robertjamespeacock@gmail.com>
2020-12-06 11:25:44 +0000

    day 6 refactor

--- day_6/app.py
+++ day_6/app.py
@@ -3,23 +3,17 @@ groups = [[]]
 
 for line in file:
     line = line.rstrip('\n')
-    if line:
-        #Add details to current group
+    if line: #Add details to current group
         groups[len(groups) - 1].append(line)    
-    else:
-        #Start a new group
+    else: #Start a new group
         groups.append([])
         
-#PART ONE
 def any_answer_yes_count(group):
     return len(set(''.join(group)))
 
-answer = sum(any_answer_yes_count(group) for group in groups)
-print(answer)
-
-#PART TWO
 def all_answer_yes_count(group):
     return len([answer for answer in set(''.join(group)) if all(answer in person for person in group)])
 
-answer = sum(all_answer_yes_count(group) for group in groups)
-print(answer)
+part_1_answer = sum(any_answer_yes_count(group) for group in groups)
+part_2_answer = sum(all_answer_yes_count(group) for group in groups)
+print("Part 1: " + part_1_answer + " Part 2: " + part_2_answer)
