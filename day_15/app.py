25b31ac8f264967ac8f901f14ae3cedbc11d0f4f
Robert Peacock <robertjamespeacock@gmail.com>
2020-12-15 22:08:13 +0000

    Update app.py
    Day 15 refactor

--- day_15/app.py
+++ day_15/app.py
@@ -1,22 +1,23 @@
 import time
 
-start_nums = [1,20,11,6,12,0]
-last_call_dict = {}
-last_number = None
+puzzle_input = [1,20,11,6,12,0]
+
+def play(start_nums, iterations):
+    last_calls = {}
+    last_num = said_num = None
+    for i in range(iterations):
+        if i < len(start_nums):
+            said_num = start_nums[i]
+        elif last_num in last_calls: 
+            said_num = i - last_calls[said_num]
+        else:
+            said_num = 0
+        if last_num != None:
+            last_calls[last_num] = i
+        last_num = said_num
+    return said_num
 
 tic = time.perf_counter()
-for i in range(1, 30000001):
-    if start_nums:
-               said_number = start_nums[0]
-               start_nums.remove(said_number)
-    elif last_number in last_call_dict:
-               said_number = i - last_call_dict[last_number]
-    else:
-               said_number = 0
-    last_call_dict[last_number] = i
-    last_number = said_number
+print(play([0,3,6], 30000000))
 toc = time.perf_counter()
-    
-print(said_number)
 print("Complete in " + str(toc - tic))    
-    
